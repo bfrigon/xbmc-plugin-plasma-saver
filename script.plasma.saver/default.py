8de93f43d89af0cbd5b91e70b721f6ed84e7ed1e
Benoit Frigon <bfrigon@gmail.com>
2015-01-12 21:29:39 -0500

    Root directory must have the same name than the addon id

--- script.plasma.saver/default.py
+++ script.plasma.saver/default.py
@@ -16,7 +16,7 @@ __plugin__ = "script.plasma.saver"
 __author__ = 'bfrigon [bfrigon@gmail.com]'
 __url__ = "www.bfrigon.com"
 __credits__ = ""
-__version__ = "0.1.006"
+__version__ = "0.1.011"
 __settings__ = xbmcaddon.Addon(id='script.plasma.saver')
 __cwd__ = __settings__.getAddonInfo('path')
 
